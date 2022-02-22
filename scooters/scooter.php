639c5dc1b1cdb02e64b315d89e169a7288c140a5
frame88 <fleuzzi97@gmail.com>
2022-02-22 21:37:10 +0100

    scooter.php & user.php

---
+++ scooters/scooter.php
@@ -0,0 +1,63 @@
+<?php
+
+class Scooter {
+
+    protected $brand;
+    protected $plate;   //targa
+    protected $displacement;    //cilindrata
+    protected $hp;  //cavalli
+    protected $productionyear;   //anno di produzione
+}
+
+public function __construct($brand, $plate)
+{
+    $this->setBrand($brand);
+    $this->setPlate($plate);
+}
+
+
+
+public function getBrand() {
+    return $this->brand;
+}
+public function setBrand($brand) {
+    $this->brand = $brand;
+    return $this;
+}
+
+public function getPlate(){
+    return $this->plate;
+}
+public function setPlate($plate){
+    $this->plate = $plate;
+    return $this;
+}
+
+public function getDisplacement(){
+    return $this->displacement;
+}
+public function setDisplacement($displacement){
+    $this->displacement = $displacement;
+    return $this;
+}
+
+public function getHp(){
+    return $this->hp;
+}
+public function setHp($hp){
+    $this->hp = $hp;
+    return $this;
+}
+
+public function getProductionyear(){
+    return $this->productionyear;
+}
+public function setProductionyear($productionyear){
+    $this->productionyear = $productionyear;
+    return $this;
+}
+
+
+
+
+?>
---
+++ scooters/user.php
@@ -0,0 +1,74 @@
+<?php 
+//creazione istanza
+class User {
+    //attributi
+    protected $name;
+    protected $lastname;
+    protected $age;
+    protected $payment;
+    protected $punti;
+}
+
+public function __construct($name, $age, $punti)
+{
+    //dati obbligatori per la creazione di un nuovo oggetto user
+    $this->setName($name);
+    $this->setAge($age);
+    $this->setPunti($punti);
+}
+
+//get   return $this->esempio;
+//set($esempio)     $this->esempio = $esempio;    return $this;  
+
+public function getName()
+{
+    return $this->name;
+}
+public function setName($name)
+{
+    $this->name = $name;
+    return $this;
+}
+
+
+public function getLastname()
+{
+    return $this->lastname;    
+}
+public function setLastname($lastname)
+{
+    $this->lastname = $lastname;
+    return $this;
+}
+
+
+public function getAge() 
+{
+    return $this->age;
+}
+public function setAge($age)
+{
+    $this->age = $age;
+    return $this;
+}
+
+public function getPayment()
+{
+    return $this->payment;
+}
+public function setPayment($payment)
+{
+    $this->payment = $payment;
+    return $this;
+}
+
+public function getPunti()
+{
+    return $this->punti;
+}
+public function setPunti($punti)
+{
+    $this->punti = $punti;
+    return $this;
+}
+?>
